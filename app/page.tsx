64b067e6d5931a763895c919e5c4d96192444c46
ShamuoonHaider <shamuoonh08@gmail.com>
2025-11-14 21:43:03 +0500

    02-customization

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
+
  
 const Page = () => {
   return (
-    <div>
-      <Input placeholder="Type something..." />
-      <Button variant="ghost">Push me</Button>
-      
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elavated">
+            Click Me
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="Type something...."/>
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="the is a text area" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   )
 }
