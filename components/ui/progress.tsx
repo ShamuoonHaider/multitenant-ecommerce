4a88d9a7b251bc2fd992c918a07ed8aa51f877c5
ShamuoonHaider <shamuoonh08@gmail.com>
2025-11-15 11:38:24 +0500

    created the nav bar with media screen as well also added to footer

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Progress({
   className,
@@ -15,18 +15,18 @@ function Progress({
       data-slot="progress"
       className={cn(
         "relative h-3 w-full overflow-hidden rounded-full",
-        "border bg-transparent",
+        "border bg-white",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        className="bg-pink-400 h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
-  )
+  );
 }
 
-export { Progress }
+export { Progress };
