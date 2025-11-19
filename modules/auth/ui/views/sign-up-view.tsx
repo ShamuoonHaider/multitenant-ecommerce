4813f07ac4f00a3e4559b27ec013ec60def794aa
ShamuoonHaider <shamuoonh08@gmail.com>
2025-11-19 16:13:51 +0500

    sign In page ui is done

---
+++ modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "../../schemas";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["700"],
+});
+
+export const SignUpView = () => {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    mode: "all",
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof registerSchema>) => {};
+
+  const username = form.watch("username");
+  const usernameErrors = form.formState.errors.username;
+
+  const showPreview = username && !usernameErrors;
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-5">
+      <div className="bg-[#F4F4F4] h-screen w-full lg:col-span-3 overflow-y-auto">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-8 p-4 lg:p-16"
+          >
+            <div className="flex items-center justify-between mb-8">
+              <Link href="/">
+                <span
+                  className={cn("text-2xl font-semibold", poppins.className)}
+                >
+                  funroad
+                </span>
+              </Link>
+              <Button
+                asChild
+                variant="ghost"
+                size="sm"
+                className="text-base border-none underline"
+              >
+                <Link prefetch href="/sign-in">
+                  Sign In
+                </Link>
+              </Button>
+            </div>
+            <h1 className="text-4xl font-medium">
+              Join over 10,000 sellers on Funroad
+            </h1>
+            <FormField
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your username" {...field} />
+                  </FormControl>
+                  <FormDescription
+                    className={cn("hidden", showPreview && "block")}
+                  >
+                    Your store will be available at&nbsp;
+                    <strong>{username}</strong>
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your email" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Your password"
+                      {...field}
+                      type="password"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="bg-black text-white hover:bg-pink-400 hover:text-primary"
+              size="lg"
+              variant="elevated"
+            >
+              Create Account
+            </Button>
+          </form>
+        </Form>
+      </div>
+      <div
+        className="h-screen w-full lg:col-span-2 lg:block"
+        style={{
+          backgroundImage: "url('/auth-bg.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+    </div>
+  );
+};
